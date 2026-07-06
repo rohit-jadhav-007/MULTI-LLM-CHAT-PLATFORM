85e1f29c2489d7d6c9bf0326f21d61f805711605
rohit-jadhav-oo7 <rohit.jadhav010404@gmail.com>
2026-07-06 14:49:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MULTI-LLM-CHAT-PLATFORM
+Spring Boot application integrating Google Gemini and Ollama models (Phi-4 Mini, Gemma, Llama) through a unified REST API using Spring AI. Supports seamless switching between cloud and local LLMs while showcasing scalable backend design, dependency injection, and AI model integration.
---
+++ springAiDemo/src/main/java/com/spring/springAiDemo/GoogleAI.java
@@ -0,0 +1,38 @@
+package com.spring.springAiDemo;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.google.genai.GoogleGenAiChatModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/google")
+@CrossOrigin("*")
+public class GoogleAI {
+
+    private ChatClient chatClient;
+
+    public GoogleAI(GoogleGenAiChatModel chatModel){
+        this.chatClient = ChatClient.create(chatModel);
+    }
+
+    @GetMapping("/{message}")
+    public ResponseEntity<String> getAnswer(@PathVariable String message){
+
+        ChatResponse chatResponse = chatClient
+                .prompt(message)
+                .call()
+                .chatResponse();
+
+        String response = chatResponse.getResult().getOutput().getText();
+
+        return ResponseEntity.ok(response);
+    }
+
+}
